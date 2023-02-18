99e3fe7366f8f55f1fa08ae738115465f8bf7f88
sungnga <nga422@gmail.com>
2023-02-18 14:31:17 -0800

    Building the Profile screen, CompletedCoursesList

--- design_code/lib/components/home_screen_navbar.dart
+++ design_code/lib/components/home_screen_navbar.dart
@@ -1,6 +1,7 @@
 import 'package:design_code/components/searchfield_widget.dart';
 import 'package:design_code/components/sidebar_button.dart';
 import 'package:design_code/constants.dart';
+import 'package:design_code/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreenNavBar extends StatelessWidget {
@@ -15,16 +16,24 @@ class HomeScreenNavBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SidebarButton(triggerAnimation: triggerAnimation,),
+          SidebarButton(
+            triggerAnimation: triggerAnimation,
+          ),
           SearchFieldWidget(),
           Icon(
             Icons.notifications,
             color: kPrimaryLabelColor,
           ),
-          SizedBox(width: 16.0),
-          CircleAvatar(
-            radius: 18.0,
-            backgroundImage: AssetImage('asset/images/profile.jpg'),
+          SizedBox(
+            width: 16.0,
+          ),
+          GestureDetector(
+            onTap: (() => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => ProfileScreen()))),
+            child: CircleAvatar(
+              radius: 18.0,
+              backgroundImage: AssetImage('asset/images/profile.jpg'),
+            ),
           ),
         ],
       ),
