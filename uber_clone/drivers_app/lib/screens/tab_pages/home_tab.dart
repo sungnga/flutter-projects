fdd007848ab35800cb13cc0785199108cc96d769
sungnga <nga422@gmail.com>
2023-02-22 22:16:44 -0800

    Drivers App: implement google map, black theme map

--- uber_clone/drivers_app/lib/screens/tab_pages/home_tab.dart
+++ uber_clone/drivers_app/lib/screens/tab_pages/home_tab.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:drivers_app/models/map_style.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({super.key});
@@ -8,10 +11,32 @@ class HomeTabPage extends StatefulWidget {
 }
 
 class _HomeTabPageState extends State<HomeTabPage> {
+  final Completer<GoogleMapController> _controllerGoogleMap =
+      Completer<GoogleMapController>();
+  GoogleMapController? newGoogleMapController;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete(controller);
+              newGoogleMapController = controller;
+              newGoogleMapController!.setMapStyle(blackThemeMapStyle);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
